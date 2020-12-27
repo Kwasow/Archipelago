e5d101d9cdf18fea75082d4a69b3e1a13b7aaa8d
Karol Wąsowski <wasowski02@gmail.com>
2020-12-27 22:57:07 +0100

    Added date picker and safety checks for inputs

--- app/src/main/java/com/github/kwasow/archipelago/activities/AddSourceActivity.kt
+++ app/src/main/java/com/github/kwasow/archipelago/activities/AddSourceActivity.kt
@@ -5,12 +5,24 @@ import android.view.View
 import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.github.kwasow.archipelago.R
+import com.github.kwasow.archipelago.data.*
 import com.github.kwasow.archipelago.databinding.ActivityAddSourceBinding
 import com.github.kwasow.archipelago.utils.CountryManager
 import com.github.kwasow.archipelago.utils.SourceManager
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddSourceActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddSourceBinding
+    private lateinit var countries: Array<Country>
+    private lateinit var caps: List<String>
+    var currentSource = 0
+    var countryChosen = false
+    var capChosen = false
+
+    var startDate: Date? = null
+    var endDate: Date? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +35,7 @@ class AddSourceActivity : AppCompatActivity() {
             onBackPressed()
         }
 
-        // Set up source chooser
+        // Set up source picker
         val sources = listOf("cash source", "savings account", "investment", "stock")
         val sourcesAdapter = ArrayAdapter(this, R.layout.list_item, sources)
         binding.sourceType.setAdapter(sourcesAdapter)
@@ -44,26 +56,18 @@ class AddSourceActivity : AppCompatActivity() {
 
             when (i) {
                 // Cash
-                0 -> {
-                    sourceCash()
-                }
+                0 -> sourceCash()
                 // Savings
-                1 -> {
-                    sourceSavings()
-                }
+                1 -> sourceSavings()
                 // Investment
-                2 -> {
-                    sourceInvestment()
-                }
+                2 -> sourceInvestment()
                 // Stock
-                3 -> {
-                    sourceStock()
-                }
+                3 -> sourceStock()
             }
         }
 
         // Set up country chooser
-        val countries = CountryManager.getCountries(this)
+        countries = CountryManager.getCountries(this)
         val countryCodes = mutableListOf<String>()
         countries.forEach {
             countryCodes.add(it.code)
@@ -73,10 +77,11 @@ class AddSourceActivity : AppCompatActivity() {
         // Set currency when country is selected
         binding.countrySelect.setOnItemClickListener { _, _, i, _ ->
             binding.amount.setCurrency(countries[i].currency)
+            countryChosen = true
         }
 
         // Set up possible capitalization options
-        val caps = listOf(
+        caps = listOf(
                 resources.getString(SourceManager.Capitalization.EndOfMonth.value),
                 resources.getString(SourceManager.Capitalization.EndOfInvestment.value),
                 resources.getString(SourceManager.Capitalization.Monthly.value),
@@ -84,6 +89,7 @@ class AddSourceActivity : AppCompatActivity() {
         )
         val capAdapter = ArrayAdapter(this, R.layout.list_item, caps)
         binding.capitalization.setAdapter(capAdapter)
+        binding.capitalization.setOnItemClickListener { _, _, _, _ -> capChosen = true }
 
         binding.interest.setCurrency("%")
 
@@ -91,15 +97,160 @@ class AddSourceActivity : AppCompatActivity() {
     }
 
     fun finishAdding(view: View) {
-        // TODO: Get and save data
+        // Check if all the needed details were provided
+        var error = false
+        if (binding.sourceName.text.toString().isBlank()) {
+            binding.sourceNameLayout.error = resources.getString(R.string.error_blank)
+            error = true
+        } else {
+            binding.sourceNameLayout.error = ""
+        }
+
+        if (!countryChosen) {
+            binding.countrySelectLayout.error = resources.getString(R.string.error_blank)
+            error = true
+        } else {
+            binding.countrySelectLayout.error = ""
+        }
+
+        if ((currentSource == 1 || currentSource == 2) && !capChosen) {
+            binding.capitalizationLayout.error = resources.getString(R.string.error_blank)
+            error = true
+        } else {
+            binding.capitalizationLayout.error = ""
+        }
+
+        if (currentSource == 2) {
+            if (binding.dateStart.text.toString().isBlank()) {
+                binding.dateStartLayout.error = resources.getString(R.string.error_blank)
+                error = true
+            } else {
+                binding.dateStartLayout.error = ""
+            }
+
+            if (binding.dateEnd.text.toString().isBlank()) {
+                binding.dateEndLayout.error = resources.getString(R.string.error_blank)
+                error = true
+            } else {
+                binding.dateEndLayout.error = ""
+            }
+
+            if (endDate != null && startDate != null) {
+                if (endDate!!.before(startDate)) {
+                    binding.dateEndLayout.error = resources.getString(R.string.error_endDate_before_startDate)
+                    error = true
+                } else if (startDate!!.before(endDate)) {
+                    binding.dateEndLayout.error = ""
+                }
+            }
+
+
+        }
+        // Don't continue if any errors were found
+        if (error) return
+
+        // Get common source attributes
+        val name = binding.sourceName.text.toString()
+        val countryTmp = CountryManager
+                .getCountyByCode(this, binding.countrySelect.text.toString())
+        val country = countryTmp.name
+        val countryCode = countryTmp.code
+        val currency = countryTmp.currency
+        val amount = binding.amount.getDoubleValue()
+
+        val transactions = listOf(
+                Transaction(
+                        Date(),
+                        resources.getString(R.string.initial_account_state),
+                        amount,
+                        resources.getString(R.string.added_automatically)
+                )
+        ).toTypedArray()
+
+        // Specific stuff
+        when (currentSource) {
+            // Cash
+            0 -> {
+                SourceCash(
+                        name,
+                        country,
+                        countryCode,
+                        currency,
+                        amount,
+                        transactions
+                ).save(this)
+            }
+            // Savings account
+            1 -> {
+                SourceAccount(
+                        name,
+                        country,
+                        countryCode,
+                        currency,
+                        amount,
+                        binding.interest.getDoubleValue(),
+                        SourceManager.Capitalization.valueOf(
+                                caps[binding.capitalization.listSelection]),
+                        transactions
+                ).save(this)
+            }
+            // Investment
+            2 -> {
+                /*
+                SourceInvestment(
+                        name,
+                        country,
+                        countryCode,
+                        currency,
+                        amount,
+                        binding.interest.getDoubleValue(),
+                        SourceManager.Capitalization.valueOf(
+                                caps[binding.capitalization.listSelection]),
+
+                ) */
+            }
+        }
+
+        finish()
+    }
+
+    // TODO: Look into pickers - they seem slow
+    fun dateStart(view: View) {
+        val picker = MaterialDatePicker.Builder.datePicker().build()
+        picker.addOnPositiveButtonClickListener {
+            val date = Date(it)
+            val dateFormat = SimpleDateFormat.getDateInstance()
+            binding.dateStart.setText(
+                    dateFormat.format(date)
+            )
+            startDate = date
+        }
+        picker.show(supportFragmentManager, picker.toString())
+    }
+
+    fun dateEnd(view: View) {
+        val picker = MaterialDatePicker.Builder.datePicker().build()
+        picker.addOnPositiveButtonClickListener {
+            val date = Date(it)
+            val dateFormat = SimpleDateFormat.getDateInstance()
+            binding.dateEnd.setText(
+                    dateFormat.format(date)
+            )
+            endDate = date
+        }
+        picker.show(supportFragmentManager, picker.toString())
     }
 
     private fun sourceCash() {
+        currentSource = 0
+
         // TODO: Maybe add animation when selecting source
         binding.photoLeading.setImageResource(R.drawable.ic_dollar)
     }
 
     private fun sourceSavings() {
+        currentSource = 1
+
         binding.photoLeading.setImageResource(R.drawable.ic_credit_card)
 
         binding.interestLayout.visibility = View.VISIBLE
@@ -107,6 +258,8 @@ class AddSourceActivity : AppCompatActivity() {
     }
 
     private fun sourceInvestment() {
+        currentSource = 2
+
         binding.photoLeading.setImageResource(R.drawable.ic_percent)
 
         binding.interestLayout.visibility = View.VISIBLE
@@ -116,6 +269,8 @@ class AddSourceActivity : AppCompatActivity() {
     }
 
     private fun sourceStock() {
+        currentSource = 3
+
         // TODO: Stocks
         binding.photoLeading.setImageResource(R.drawable.ic_graph)
 
